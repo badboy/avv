617d5b2453fc4ecdee1df3a26dfc11b30b9e8a5a
Jan-Erik Rediger <badboy@archlinux.us>
2011-11-30 20:45:20 +0100

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+# avv
+
+avv is a commandline client for the http://fahrplan.avv.de/ interface. It requests bus connections inside the AVV area.
+
+## Basic usage
+
+Find a connection from point A to point B:
+
+    avv [from-city] from-stop [to-city] to-stop
+
+When no city is specified, Aachen is assumed (as I live there now).
+
+Show departures from a given stop in Aachen:
+
+    avv from-stop
+
+Do not want to start immediately? Just pass a time:
+
+    avv from-stop -t 14:00
+
+## Install
+
+First you need [mechanize](http://mechanize.rubyforge.org/):
+
+    gem install mechanize
+
+Then copy the `avv.rb` file to a folder of your `$PATH` and you're ready to use it.
+
+## License
+
+Copyright (c) 2011 Jan-Erik Rediger <http://fnordig.de/about/>
+
+Permission  is  hereby granted, free of charge, to any person ob-
+taining a copy of  this  software  and  associated  documentation
+files  (the "Software"), to deal in the Software without restric-
+tion, including without limitation the rights to use, copy, modi-
+fy, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is  fur-
+nished to do so, subject to the following conditions:
+
+The  above  copyright  notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF  ANY  KIND,
+EXPRESS  OR  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE  AND  NONIN-
+FRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER  IN  AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN  THE
+SOFTWARE.
---
+++ avv.rb
@@ -0,0 +1,118 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'mechanize'
+
+AVV_FROM_TO="http://fahrplan.avv.de/master.php?&script=inquiry/service&apikey=avvmobil&path=%2Fhome%2FconnectionSearchForm"
+AVV_FROM="http://fahrplan.avv.de/master.php?&script=inquiry/service&apikey=avvmobil&path=%2Fhome%2FdepartureSearchForm"
+
+city = "aachen"
+departure = "am hügel"
+
+def help
+  f = File.basename $0
+  puts <<-EOF
+usage: 
+  #{f} [from-city] from-stop [to-city] to-stop
+  #{f} from-stop
+  
+  When no city is specified, Aachen is assumed.
+  
+Options:
+  -t | --time\tJourney start time
+  EOF
+end
+
+def departure_list(city, departure, start_at)
+  a = Mechanize.new
+  a.get(AVV_FROM) do |page|
+    result_page = page.form_with(:action => 'http://fahrplan.avv.de/master.php') do |f|
+      f.dep_city    = city
+      f.dep_station = departure
+      f.time = start_at if start_at
+    end.submit
+
+    departure = result_page.search('span.blueText b').text
+    puts departure
+    puts '-'*departure.size
+
+    results = result_page.search('table.departure tr')
+    results.each do |row|
+      if row.text =~ /ab\nLinie/
+        #puts "Uhrzeit\tLinie\tFahrtziel"
+        #puts "-------------------------"
+        next
+      end
+      res = []
+      row.search('td').each do |col|
+        res << col.text
+      end
+      puts res*"\t"
+    end
+  end
+end
+
+def from_to_list(departure, start_city, target, target_city=nil, start_at=nil)
+  target_city = start_city if target_city.nil?
+
+  a = Mechanize.new
+  a.get(AVV_FROM_TO) do |page|
+    result_page = page.form_with(:action => 'http://fahrplan.avv.de/master.php') do |f|
+      f.dep_city    = start_city
+      f.dep_station = departure
+      f.arr_city    = target_city
+      f.arr_station = target
+      f.time = start_at if start_at
+    end.submit
+
+    text = result_page.search('span.blueText').text.gsub("\n", ' ')
+      .strip.gsub(/  +/, ' ')
+    puts text
+    puts '-'*text.size
+
+    results = result_page.search('table.verbindungsuebersicht tr')
+    results.each do |row|
+      if row.text =~ /Linie\(n\)/
+        puts "Linie\tab\tUm.\tan"
+        puts "-----------------------------"
+        next
+      end
+      res = []
+      tds = row.search('td')
+      next if tds.size == 1 || tds.size == 0
+      tds.each do |col|
+        t = col.text.chomp.strip
+        next if t =~ /Details/
+        res << t
+      end
+      puts res*"\t"
+    end
+  end
+end
+
+start_at=nil
+if d = ARGV.index('--time') || d = ARGV.index('-t')
+  if ARGV[d+1]
+    start_at = ARGV[d+1]
+
+    ARGV.delete_at(d)
+    ARGV.delete_at(d)
+  else
+    "Da fehlt aber was."
+    exit 1
+  end
+end
+
+if ARGV.empty? || ARGV.include?('-h') || ARGV.include?('--help')
+  help
+  exit 1
+end
+
+case ARGV.size
+when 1 then
+    departure_list('aachen', ARGV[0], start_at)
+when 2 then
+    from_to_list(ARGV[0], 'aachen', ARGV[1], 'aachen', start_at)
+else
+  puts "Keine Ahnung, was zu tun ist."
+end
